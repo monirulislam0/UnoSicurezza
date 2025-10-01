81669e48334b52b02b72bb565a0e69ac13f33717
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-10-01 14:06:14 +0600

    No Commenbts

---
+++ app.js
@@ -0,0 +1,19 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+
+//   var swiper = new Swiper(".mySwiper", {
+
+//       slidesPerView: 2,
+//       spaceBetween: 0,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+
+      
+//     });
+
